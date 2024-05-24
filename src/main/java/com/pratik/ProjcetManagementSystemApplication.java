0a93100108430b0366b51951f6359672196da5fe
Pratik <pratikyadav3949@gmail.com>
2024-05-24 09:47:51 +0530

    first commit

---
+++ src/main/java/com/pratik/ProjcetManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.pratik;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjcetManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjcetManagementSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pratik/ProjcetManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pratik;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjcetManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
