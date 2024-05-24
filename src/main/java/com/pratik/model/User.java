dd321ff974230aa48d4451d1091c44fd959eb95c
Pratik <pratikyadav3949@gmail.com>
2024-05-24 11:35:53 +0530

    added spring security

--- src/main/java/com/pratik/model/User.java
+++ src/main/java/com/pratik/model/User.java
@@ -1,2 +1,26 @@
-package com.pratik.model;public class User {
+package com.pratik.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Data
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String fullName;
+    private String email;
+    private String password;
+
+    @JsonIgnore
+    @OneToMany(mappedBy = "assignee",cascade = CascadeType.ALL)
+    private List<Issue> assignedIssues = new ArrayList<>();
+
+    private int projectSize;
 }
