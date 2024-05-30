d8c812bb447b63cd233b7b16f7194dd3c88d298c
Pratik <pratikyadav3949@gmail.com>
2024-05-30 11:47:28 +0530

    Implementing subscription feature

---
+++ src/main/java/com/pratik/model/PlanType.java
@@ -0,0 +1,7 @@
+package com.pratik.model;
+
+public enum PlanType {
+    FREE,
+    MONTHLY,
+    ANNUALLY
+}
---
+++ src/main/java/com/pratik/model/Subscription.java
@@ -0,0 +1,28 @@
+package com.pratik.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Subscription {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private LocalDate subscriptionStartDate;
+    private LocalDate getSubscriptionEndDate;
+
+    private PlanType planType;
+
+    private boolean isValid;
+
+    @OneToOne
+    private User user;
+}
