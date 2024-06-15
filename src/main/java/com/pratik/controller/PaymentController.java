7b72dc50612ec6b9a7e2d0807618db70e01a7a06
Pratik <pratikyadav3949@gmail.com>
2024-06-15 18:21:28 +0530

    changed some endpoints

--- src/main/java/com/pratik/controller/PaymentController.java
+++ src/main/java/com/pratik/controller/PaymentController.java
@@ -14,7 +14,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/api/payment")
+@RequestMapping("/api/payments")
 public class PaymentController {
 
     @Value("${razorpay.api.key}")
