df5a413e04d686ce6271b95d7d21630185b3aa07
Pratik Yadav <114671066+Pycreater@users.noreply.github.com>
2025-01-18 21:07:13 +0530

    Update PaymentController.java

--- src/main/java/com/pratik/controller/PaymentController.java
+++ src/main/java/com/pratik/controller/PaymentController.java
@@ -26,6 +26,7 @@ public class PaymentController {
     @Autowired
     private UserService userService;
 
+    //hello there
     @PostMapping("/{planType}")
     public ResponseEntity<PaymentLinkResponse> createPaymentLink(
             @PathVariable PlanType planType,
